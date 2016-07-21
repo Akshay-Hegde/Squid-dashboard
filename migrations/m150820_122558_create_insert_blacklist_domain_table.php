06e887e2a4c038e9c85e74dc10668629695591ea
Alexander Phinikarides <alexisph@gmail.com>
2016-07-21 15:11:35 +0300

    refactored the blacklist import (speedup by a factor of 10^4)

--- migrations/m150820_122558_create_insert_blacklist_domain_table.php
+++ migrations/m150820_122558_create_insert_blacklist_domain_table.php
@@ -5,160 +5,76 @@ use yii\db\Migration;
 
 class m150820_122558_create_insert_blacklist_domain_table extends Migration
 {
-	public function safeUp()
-	{
-		$this->createTable('blacklist_domains', [
-				'id' => Schema::TYPE_PK,
-				'domain' => Schema::TYPE_STRING . ' NOT NULL',
-				'blacklist_id' => Schema::TYPE_INTEGER . ' NOT NULL',
-		],'ENGINE=InnoDB');
+  public function safeUp()
+  {
+    $this->createTable('blacklist_domains', [
+      'id' => Schema::TYPE_PK,
+      'domain' => Schema::TYPE_STRING . ' NOT NULL',
+      'blacklist_id' => Schema::TYPE_INTEGER . ' NOT NULL',
+    ],'ENGINE=InnoDB');
 
-		$this->addForeignKey('FK_BL_DOMAIN_BL_ID','blacklist_domains','blacklist_id','blacklist','id','CASCADE','CASCADE');
+    $this->addForeignKey('FK_BL_DOMAIN_BL_ID','blacklist_domains','blacklist_id','blacklist','id','CASCADE','CASCADE');
 
+    // Limit bulk inserts to 10000 rows at a time
+    $maxRecord = 10000;
+    $recordNum = 0;
 
-		// Limit bulk inserts to 10000 rows at a time
-		$maxRecord = 10000;
-		$recordNum = 0;
+    // Define the blacklist types
+    $blType = [
+      ['adult', 1],
+      ['aggressive', 2],
+      ['audio-video', 4],
+      ['drugs', 5],
+      ['mixed_adult', 6],
+      ['violence', 7],
+      ['porn', 8],
+      ['ads', 9],
+      ['dating', 10],
+      ['chat', 11],
+      ['gambling', 12]
+    ];
 
-		// Import the blacklists
-		$handle = fopen("/etc/squidguard/blacklists/adult/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			$recordNum = 0;
-			while (($line = fgets($handle)) !== false) {
-				++$recordNum;
-				$bulkInsertArray[] = ['', trim($line), '1'];
-				if ($recordNum == $maxRecord) {
-					$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-					$bulkInsertArray = [];
-					$recordNum = 0;
-				}
-			}
-			if ($recordNum < 1000 & $recordNum > 0) {
-				$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-			}
-			fclose($handle);
-		}
+    // Iterate over the blacklists and bulk insert into the DB
+    foreach($blType as $type)
+    {
+      $handle = fopen("/etc/squidguard/blacklists/" . $type[0] . "/domains", "r");
+      if ($handle) {
+        $bulkInsertArray = [];
+        $recordNum = 0;
+        while (($line = fgets($handle)) !== false) {
+          ++$recordNum;
+          $bulkInsertArray[] = ['', trim($line), $type[1]];
+          if ($recordNum == $maxRecord) {
+            $this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
+            $bulkInsertArray = [];
+            $recordNum = 0;
+          }
+        }
+        if ($recordNum < 1000 & $recordNum > 0) {
+          $this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
+        }
+        fclose($handle);
+      }
+    }
+    return true;
+  }
 
-		$handle = fopen("/etc/squidguard/blacklists/aggressive/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '2'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
+  public function down()
+  {
+    echo "m150805_133741_insert_blacklists TRUNCATED.\n";
+    $this->truncateTable('blacklist_domains');
 
-		$handle = fopen("/etc/squidguard/blacklists/audio-video/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '4'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
+    return true;
+  }
 
-		$handle = fopen("/etc/squidguard/blacklists/drugs/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '5'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
 
-		$handle = fopen("/etc/squidguard/blacklists/mixed_adult/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '6'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/violence/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '7'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/porn/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '8'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/ads/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '9'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/dating/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '10'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/chat/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '11'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		$handle = fopen("/etc/squidguard/blacklists/gambling/domains", "r");
-		if ($handle) {
-			$bulkInsertArray = [];
-			while (($line = fgets($handle)) !== false) {
-				$bulkInsertArray[] = ['', trim($line), '12'];
-			}
-			fclose($handle);
-			$this->batchInsert('blacklist_domains', ['id', 'domain', 'blacklist_id'], $bulkInsertArray);
-		}
-
-		return true;
-
-	}
-
-	public function down()
-	{
-		echo "m150805_133741_insert_blacklists cannot be reverted.\n";
-
-		return false;
-	}
-
-	/*
-	// Use safeUp/safeDown to run migration code within a transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-    */
+    public function safeDown()
+    {
+    }
+     */
 }
